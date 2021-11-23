abda6af737ad52eefd040a90c1377e11d6a1d2c0
Sereza7 <lucas.charpentier@imt-atlantique.net>
2021-11-23 14:51:36 +0900

    Updated model
    And added a basic starting scene

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+	//Rotational pulse
+	private float speed = 50f;
+	private float pulseX;
+	private float pulseY;
+	private float pulseZ;
+	private int tick = 0;
+	private float shiftX;
+	private float shiftY;
+	private float shiftZ;
+
+	private void Start()
+	{
+		pulseX = Random.Range(1/speed, 5/speed);
+		pulseY = Random.Range(1 / speed, 5 / speed);
+		pulseZ = Random.Range(1 / speed, 5 / speed);
+		shiftX = Random.Range(0, 6.28f);
+		shiftY = Random.Range(0, 6.28f);
+		shiftZ = Random.Range(0, 6.28f);
+	}
+
+	void Update()
+	{
+		tick++;
+		transform.Rotate(Time.deltaTime * speed * Mathf.Sin(shiftX + 0.01f * tick * pulseX), 0, 0, Space.Self);
+		transform.Rotate(0, Time.deltaTime * speed * Mathf.Sin(shiftY + 0.01f * tick * pulseY), 0, Space.Self);
+		transform.Rotate(0, 0, Time.deltaTime * speed * Mathf.Sin(shiftZ + 0.01f * tick * pulseZ), Space.Self);
+	}
+}
