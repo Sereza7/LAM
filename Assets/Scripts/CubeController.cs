806a26c9fb084fed6ff53ee8cf07d0924d32a614
Sereza7 <lucas.charpentier@imt-atlantique.net>
2021-11-30 20:14:48 +0900

    Added the physics puzzle model
    And a basic scene to test it out

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+	private float sensitivity = 1f;
+	private bool rotating = false;
+
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+
+			if (touch.phase == TouchPhase.Began)
+			{
+				rotating = Physics.Raycast(Camera.main.ScreenPointToRay(touch.position), out RaycastHit hit);
+			}
+			if (touch.phase == TouchPhase.Moved && this.rotating)
+			{
+				float h = sensitivity * touch.deltaPosition.x * touch.deltaTime;
+				float v = sensitivity * touch.deltaPosition.y * touch.deltaTime;
+
+				
+				this.transform.Rotate(-this.transform.forward, v);
+				this.transform.Rotate(-this.transform.up, h);
+
+			}
+		}
+	}
+}
