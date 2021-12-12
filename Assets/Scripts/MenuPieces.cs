64ae72af8d9f5df05aa44e5ad776f681922c920b
mirar <mira.razaf@gmail.com>
2021-12-12 23:41:59 +0100

    Ajout UI sélection par clic sur boutons des pièces. Test effectué uniquement sur la scène LinksPuzzle.

---
+++ Assets/Scripts/MenuPieces.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPieces : MonoBehaviour
+{
+    public RectTransform menuPanel; 
+    public Button buttonLeft, buttonRight; 
+
+    public int NbDePieces; 
+    public GameObject conteneurDesPieces; 
+    private PiecePuzzleUI[] listedepieces; 
+
+    int i = 0; 
+
+    // Awake is called before Start 
+    void Awake() { 
+        PiecePuzzleUI[] listedepiecesPPUI = new PiecePuzzleUI[NbDePieces]; 
+        for (int index = 0; index < NbDePieces; index++) { 
+            listedepiecesPPUI[index] = conteneurDesPieces.transform.GetChild(index).gameObject.GetComponent<PiecePuzzleUI>(); 
+        }
+        listedepieces = listedepiecesPPUI; 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonLeft.onClick.AddListener(VersLaGauche); 
+        buttonRight.onClick.AddListener(VersLaDroite);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i = Mathf.Clamp(i,0,NbDePieces-1); 
+        for (int j = 0; j < listedepieces.Length; j++) { 
+            listedepieces[j].NotToBeSelected();
+        }
+        listedepieces[i].ToBeSelected();  
+    }
+
+    void VersLaGauche() 
+    { 
+        i -= 1; 
+    }
+
+    void VersLaDroite() 
+    { 
+        i += 1; 
+    }
+}
---
+++ Assets/Scripts/PiecePuzzleUI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+[RequireComponent(typeof(Button))]
+public class PiecePuzzleUI : MonoBehaviour
+{
+    private RawImage displayimg; 
+    private Button displayButton; 
+    public GameObject solidPiece; 
+    public GameObject clonePiece; 
+    public Camera cameraUI; 
+
+    void Awake() { 
+        displayimg = GetComponent<RawImage>(); 
+        displayButton = GetComponent<Button>(); 
+        displayimg.enabled = false; 
+        solidPiece.SetActive(false); 
+        clonePiece.SetActive(false); 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayButton.onClick.AddListener(AddPieceToGame); 
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    public void ToBeSelected() { 
+        displayimg.enabled = true; 
+        if (!solidPiece.activeSelf) {
+            clonePiece.SetActive(true); 
+        }
+    }
+
+    public void NotToBeSelected() { 
+        displayimg.enabled = false; 
+        clonePiece.SetActive(false); 
+    }
+
+    void AddPieceToGame() { 
+        solidPiece.SetActive(!solidPiece.activeSelf); 
+        clonePiece.SetActive(false); 
+    }
+    
+}
